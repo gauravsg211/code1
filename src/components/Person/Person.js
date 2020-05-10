98b0cbafe89c6ab75c0db7c67802431d06a80d64
gaurav.singh <gaurav.singh@idc.bedbath.com>
2020-05-11 04:13:49 +0530

    made few changes

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -8,6 +8,7 @@ class person extends Component{
        <h6>Card Number:{this.props.card_no}</h6>
    <p onClick={this.props.click}>i am {this.props.age} year old</p>
    <h1>{this.props.name}</h1>
+   <h1>lets hope that my future in react development will be high, am not putting much effort in this as this game is tuffer</h1>
   
    <input type='text' onChange={this.props.changed} value={this.props.name}></input>
    <div className="link1">
